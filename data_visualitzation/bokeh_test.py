ad4b8a5603de2573613f884921c13b5e0962bf9b
rijin <rijinct@gmail.com>
2021-08-05 14:35:18 +0800

    Adding TKI code

---
+++ data_visualitzation/bokeh_test.py
@@ -0,0 +1,12 @@
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
+
+plot = figure(plot_width=400, tools="box_zoom")
+plot.circle([1,2,3,4,5], [8,6,5,2,3])
+plot.circle(x=10, y=[2,5,8,12], size=[10,20,30,40])
+
+output_file('circle.html')
+show(plot)
+
+
+
---
+++ tki/constants.py
@@ -0,0 +1,6 @@
+b_pay = 103000
+tax_flat_45k_120k = 5092
+tax_45_slab =  45000
+tax_45_remain_each_dol = 0.325
+tax_normal_py = 314
+super_an = 197.44
